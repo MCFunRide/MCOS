9b546a3ac70f522bbf532ce42e9b23d9784d19ca
Christopher P. Sacchi <christophersacchi@gmail.com>
2015-04-26 10:43:05 -0400

    +Added GPL v3.0 licensing information to the source code.

--- boot.s
+++ boot.s
@@ -1,3 +1,22 @@
+/*
+ * MCOS - Combining an OS and a website in one
+ * Copyright (C) 2015 Christopher P. Sacchi and Andersen A.G. Graham
+ *
+ * MCOS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MCOS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MCOS.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 .global boot
 .extern kmain
 
